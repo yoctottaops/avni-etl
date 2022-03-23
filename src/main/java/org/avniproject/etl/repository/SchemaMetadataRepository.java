b10e07f6cd5fb9e3e5262e2451af515409f0e9cf
Vinay Venu <ijkvinay@gmail.com>
2022-03-23 15:07:19 +0530

    Move tablemetadata methods to its own class

--- src/main/java/org/avniproject/etl/repository/SchemaMetadataRepository.java
+++ src/main/java/org/avniproject/etl/repository/SchemaMetadataRepository.java
@@ -1,17 +1,13 @@
 package org.avniproject.etl.repository;
 
-import org.avniproject.etl.domain.ContextHolder;
 import org.avniproject.etl.domain.metadata.SchemaMetadata;
 import org.avniproject.etl.domain.metadata.TableMetadata;
 import org.avniproject.etl.domain.metadata.diff.Diff;
 import org.avniproject.etl.repository.rowMappers.TableMetadataMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,12 +18,12 @@ import static org.avniproject.etl.repository.JdbcContextWrapper.runInOrgContext;
 public class SchemaMetadataRepository {
     private final JdbcTemplate jdbcTemplate;
 
-    private final ColumnMetadataRepository columnMetadataRepository;
+    private final TableMetadataRepository tableMetadataRepository;
 
     @Autowired
-    public SchemaMetadataRepository(JdbcTemplate jdbcTemplate, ColumnMetadataRepository columnMetadataRepository) {
+    public SchemaMetadataRepository(JdbcTemplate jdbcTemplate, TableMetadataRepository tableMetadataRepository) {
         this.jdbcTemplate = jdbcTemplate;
-        this.columnMetadataRepository = columnMetadataRepository;
+        this.tableMetadataRepository = tableMetadataRepository;
     }
 
     public SchemaMetadata getNewSchemaMetadata() {
@@ -97,55 +93,7 @@ public class SchemaMetadataRepository {
         schemaMetadata.setTableMetadata(
                 schemaMetadata.getTableMetadata()
                         .stream()
-                        .map(this::save)
+                        .map(tableMetadataRepository::save)
                         .collect(Collectors.toList()));
-    }
-
-    public TableMetadata save(TableMetadata tableMetadata) {
-        return columnMetadataRepository.saveColumns(
-                saveTable(tableMetadata));
-    }
-
-    private TableMetadata saveTable(TableMetadata tableMetadata) {
-        return tableMetadata.getId() == null ?
-                insert(tableMetadata) :
-                update(tableMetadata);
-    }
-
-    public TableMetadata update(TableMetadata tableMetadata) {
-        String sql = "update table_metadata\n" +
-                "set name              = :name,\n" +
-                "    type              = :type,\n" +
-                "    subject_type_id   = :subject_type_id,\n" +
-                "    program_id        = :program_id,\n" +
-                "    encounter_type_id = :encounter_type_id,\n" +
-                "    form_id           = :form_id\n" +
-                "where id = :id;";
-
-        new NamedParameterJdbcTemplate(jdbcTemplate).update(sql, addParameters(tableMetadata));
-        return tableMetadata;
-    }
-
-    public TableMetadata insert(TableMetadata tableMetadata) {
-        Number id = new SimpleJdbcInsert(jdbcTemplate).withTableName("table_metadata")
-                .usingGeneratedKeyColumns("id")
-                .executeAndReturnKey(addParameters(tableMetadata));
-
-        tableMetadata.setId(id.intValue());
-        return tableMetadata;
-    }
-
-    private Map<String, Object> addParameters(TableMetadata tableMetadata) {
-        Map<String, Object> parameters = new HashMap<>(1);
-        parameters.put("id", tableMetadata.getId());
-        parameters.put("db_user", ContextHolder.getDbUser());
-        parameters.put("name", tableMetadata.getName());
-        parameters.put("type", tableMetadata.getType().toString());
-        parameters.put("subject_type_id", tableMetadata.getSubjectTypeId());
-        parameters.put("program_id", tableMetadata.getProgramId());
-        parameters.put("encounter_type_id", tableMetadata.getEncounterTypeId());
-        parameters.put("form_id", tableMetadata.getFormId());
-
-        return parameters;
     }
 }
---
+++ src/main/java/org/avniproject/etl/repository/TableMetadataRepository.java
@@ -0,0 +1,73 @@
+package org.avniproject.etl.repository;
+
+import org.avniproject.etl.domain.ContextHolder;
+import org.avniproject.etl.domain.metadata.TableMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class TableMetadataRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    private final ColumnMetadataRepository columnMetadataRepository;
+
+    @Autowired
+    public TableMetadataRepository(JdbcTemplate jdbcTemplate, ColumnMetadataRepository columnMetadataRepository) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.columnMetadataRepository = columnMetadataRepository;
+    }
+
+    public TableMetadata save(TableMetadata tableMetadata) {
+        return columnMetadataRepository.saveColumns(
+                saveTable(tableMetadata));
+    }
+
+    private TableMetadata saveTable(TableMetadata tableMetadata) {
+        return tableMetadata.getId() == null ?
+                insert(tableMetadata) :
+                update(tableMetadata);
+    }
+
+    public TableMetadata update(TableMetadata tableMetadata) {
+        String sql = "update table_metadata\n" +
+                "set name              = :name,\n" +
+                "    type              = :type,\n" +
+                "    subject_type_id   = :subject_type_id,\n" +
+                "    program_id        = :program_id,\n" +
+                "    encounter_type_id = :encounter_type_id,\n" +
+                "    form_id           = :form_id\n" +
+                "where id = :id;";
+
+        new NamedParameterJdbcTemplate(jdbcTemplate).update(sql, addParameters(tableMetadata));
+        return tableMetadata;
+    }
+
+    public TableMetadata insert(TableMetadata tableMetadata) {
+        Number id = new SimpleJdbcInsert(jdbcTemplate).withTableName("table_metadata")
+                .usingGeneratedKeyColumns("id")
+                .executeAndReturnKey(addParameters(tableMetadata));
+
+        tableMetadata.setId(id.intValue());
+        return tableMetadata;
+    }
+
+    private Map<String, Object> addParameters(TableMetadata tableMetadata) {
+        Map<String, Object> parameters = new HashMap<>(1);
+        parameters.put("id", tableMetadata.getId());
+        parameters.put("db_user", ContextHolder.getDbUser());
+        parameters.put("name", tableMetadata.getName());
+        parameters.put("type", tableMetadata.getType().toString());
+        parameters.put("subject_type_id", tableMetadata.getSubjectTypeId());
+        parameters.put("program_id", tableMetadata.getProgramId());
+        parameters.put("encounter_type_id", tableMetadata.getEncounterTypeId());
+        parameters.put("form_id", tableMetadata.getFormId());
+
+        return parameters;
+    }
+}
