ee63f5b1d76140963dedffccef6740df9a5ef3f5
Rudresh Shrotriya <rudresh@coloredcow.com>
2023-04-03 13:42:50 +0530

    updated test cases for media api

--- src/test/java/org/avniproject/etl/controller/MediaControllerTest.java
+++ src/test/java/org/avniproject/etl/controller/MediaControllerTest.java
@@ -12,13 +12,13 @@ import static org.mockito.Mockito.*;
 public class MediaControllerTest {
 
     @Test
-    @Sql({"/test-data-teardown.sql", "/test-data.sql"})
+    @Sql({"/test-data-teardown.sql", "/test-data-for-api.sql"})
     @Sql(scripts = {"/test-data-teardown.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void shouldGetAllIfEntityFromMediaTable() {
         MediaService mediaService = mock(MediaService.class);
-        when(mediaService.list("orgb", 5, 0)).thenReturn(new ArrayList<>());
+        when(mediaService.list("orga", 5, 0)).thenReturn(new ArrayList<>());
         MediaController mediaController = new MediaController(mediaService);
-        mediaController.getMedia("orgb", "orgb", 5, 0);
+        mediaController.getMedia("orga", "orga", 5, 0);
         verify(mediaService);
     }
 
