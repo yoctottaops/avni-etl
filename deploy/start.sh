e4d1d0e583552eb148530f7ec903795f7587f68a
himeshr <himeshl@samanvayfoundation.org>
2023-04-07 19:34:36 +0530

    #525 (avni-server) | Modify start.sh to prevent triggering more than one etl job at any given point of time

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,4 +3,13 @@ if [ -z "$1" ];
   then echo "Organization Schema names filter isn't specified";
   else echo "Organization Schema names specified: $1";
 fi
-nohup java -Dlogging.file=/opt/avni-etl/avni.log -Dlogging.path=/opt/avni-etl/ -Dlogging.file.max-size=5mb -Xmx250m -XX:ErrorFile=/opt/avni-etl/jvm.log -jar /opt/avni-etl/avni-etl.jar $1 >> /opt/avni-etl/etl.log 2>&1
+
+if [ ! -e /opt/avni-etl/etl.flg ]; then
+	touch /opt/avni-etl/etl.flg;
+	printf 'ETL Job Starting, at %s\n' "$(date)" >> /opt/avni-etl/log/etl-trigger.log
+	nohup java -Dlogging.file=/opt/avni-etl/log/avni.log -Dlogging.path=/opt/avni-etl/log/ -Dlogging.file.max-size=5mb -Xmx250m -XX:ErrorFile=/opt/avni-etl/log/jvm.log -jar /opt/avni-etl/avni-etl.jar $1 >> /opt/avni-etl/log/etl.log 2>&1;
+	rm /opt/avni-etl/etl.flg;
+	printf 'ETL Job ended, at %s\n' "$(date)" >> /opt/avni-etl/log/etl-trigger.log
+else
+	printf 'ETL Job already in-prorgess, Not triggering it again. %s\n' "$(date)" >> /opt/avni-etl/log/etl-trigger.log
+fi
