99f3290f072ee6451c099f11a0e06e9788ced3a2
Joy A <itsjoy@gmail.com>
2023-06-20 14:23:11 +0530

    #0 | Fix tests

--- src/main/java/org/avniproject/etl/config/RestServiceConfiguration.java
+++ src/main/java/org/avniproject/etl/config/RestServiceConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class RestServiceConfiguration {
@@ -19,5 +20,10 @@ public class RestServiceConfiguration {
                 registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedHeaders("*").allowedMethods("*");
             }
         };
+    }
+
+    @Bean(name = "mvcHandlerMappingIntrospector")
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+        return new HandlerMappingIntrospector();
     }
 }
