a355d0c7f88d667089ceee5a784f95a951b83e94
Avinash Ramachandruni <ce16mtech11029@iith.ac.in>
2023-08-03 07:42:02 +0530

    Added 3 endpoints for hr tab analytics that can pull data from org schema - deviceModels, appVersions, userDetails

--- src/main/java/org/avniproject/etl/repository/ReportRepository.java
+++ src/main/java/org/avniproject/etl/repository/ReportRepository.java
@@ -1,8 +1,11 @@
 package org.avniproject.etl.repository;
 
+import org.avniproject.etl.dto.AggregateReportResult;
 import org.avniproject.etl.dto.UserActivityDTO;
+import org.avniproject.etl.repository.rowMappers.reports.AggregateReportMapper;
 import org.avniproject.etl.repository.rowMappers.reports.UserActivityMapper;
 import org.avniproject.etl.repository.rowMappers.reports.UserCountMapper;
+import org.avniproject.etl.repository.rowMappers.reports.UserDetailsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -60,7 +63,7 @@ public class ReportRepository {
                 "         left join ${schemaName}.encounters e on e.last_modified_by_id = u.id\n" +
                 "         left join ${schemaName}.enrolments enl on enl.last_modified_by_id = u.id\n" +
                 "         left join ${schemaName}.program_encounters enc on enc.last_modified_by_id = u.id\n" +
-                "where u.is_voided = false and u.organisation_id notnull\n" +
+                "where (u.is_voided = false or u.is_voided isnull) and u.organisation_id notnull\n" +
                 "       and coalesce(coalesce(registration_count, 0) + coalesce(encounter_count, 0) + coalesce(enrolment_count, 0) +\n" +
                 "                coalesce(program_encounter_count, 0), 0) > 0\n" +
                 "      ${userWhere}\n" +
@@ -93,5 +96,65 @@ public class ReportRepository {
                 .replace("${syncTelemetryWhere}", syncTelemetryWhere)
                 .replace("${userWhere}", userWhere);
         return jdbcTemplate.query(query, new UserCountMapper());
+    }
+
+    public List<AggregateReportResult> generateUserAppVersions(String orgSchemaName, String userWhere) {
+        String baseQuery = "select app_version as indicator,\n" +
+                "       count(*)     as count\n" +
+                "from ${schemaName}.users u\n" +
+                "         join\n" +
+                "     (select user_id,\n" +
+                "             app_version,\n" +
+                "             row_number() over (partition by user_id order by sync_start_time desc ) as rn\n" +
+                "      from ${schemaName}.sync_telemetry) l on l.user_id = u.id and rn = 1\n" +
+                "where (u.is_voided = false or u.is_voided isnull) and u.organisation_id notnull\n" +
+                "${userWhere}\n"+
+                "group by app_version;";
+        String query = baseQuery
+                .replace("${schemaName}", orgSchemaName)
+                .replace("${userWhere}", userWhere);
+        return jdbcTemplate.query(query, new AggregateReportMapper());
+    }
+
+    public List<AggregateReportResult> generateUserDeviceModels(String orgSchemaName, String userWhere) {
+        String baseQuery = "select device_model as indicator,\n" +
+                "       count(*)     as count\n" +
+                "from ${schemaName}.users u\n" +
+                "         join\n" +
+                "     (select user_id,\n" +
+                "             coalesce(device_info ->> 'brand', device_name)                          as device_model,\n" +
+                "             row_number() over (partition by user_id order by sync_start_time desc ) as rn\n" +
+                "      from ${schemaName}.sync_telemetry) l on l.user_id = u.id and rn = 1\n" +
+                "where (u.is_voided = false or u.is_voided isnull) and u.organisation_id notnull \n" +
+                "${userWhere}\n"+
+                "group by device_model;";
+        String query = baseQuery
+                .replace("${schemaName}", orgSchemaName)
+                .replace("${userWhere}", userWhere);
+        return jdbcTemplate.query(query, new AggregateReportMapper());
+    }
+
+    public List<UserActivityDTO> generateUserDetails(String orgSchemaName, String userWhere) {
+        String baseQuery = "select coalesce(u.name, u.username) as name,\n" +
+                "       app_version,\n" +
+                "       device_model,\n" +
+                "       sync_start_time\n" +
+                "from ${schemaName}.users u\n" +
+                "         join\n" +
+                "     (select user_id,\n" +
+                "             app_version,\n" +
+                "             coalesce(device_info ->> 'brand', device_name)                          as device_model,\n" +
+                "             sync_start_time,\n" +
+                "             row_number() over (partition by user_id order by sync_start_time desc ) as rn\n" +
+                "      from ${schemaName}.sync_telemetry\n" +
+                "      where sync_status = 'complete') l on l.user_id = u.id and rn = 1\n" +
+                "where (u.is_voided = false or u.is_voided isnull)\n" +
+                "  and u.organisation_id notnull\n" +
+                "  ${userWhere}\n"+
+                "order by 1 desc;";
+        String query = baseQuery
+                .replace("${schemaName}", orgSchemaName)
+                .replace("${userWhere}", userWhere);
+        return jdbcTemplate.query(query, new UserDetailsMapper());
     }
 }
