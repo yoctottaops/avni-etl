795788f5ef05306038e54285cb9e558e04bc5741
Vinay Venu <ijkvinay@gmail.com>
2023-08-17 17:11:56 +0530

    #65 | Make sql parameterized

--- src/main/java/org/avniproject/etl/repository/UserRepository.java
+++ src/main/java/org/avniproject/etl/repository/UserRepository.java
@@ -5,44 +5,21 @@ import org.avniproject.etl.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.lang.String.format;
+import static org.avniproject.etl.repository.sql.SqlFile.readFile;
 
 @Repository
 public class UserRepository {
-    private final String FIND_BY_UUID = "select u.id,\n" +
-            "       u.username username,\n" +
-            "       u.uuid uuid,\n" +
-            "       u.organisation_id organisation_id,\n" +
-            "       case when count(p.id) > 0 or bool_or(g.has_all_privileges) = true then true else false end as has_analytics_access,\n" +
-            "       case when count(aa.id) > 0 then true else false end                                        as is_admin\n" +
-            "from users u\n" +
-            "         left join account_admin aa on u.id = aa.admin_id\n" +
-            "         left join user_group ug on u.id = ug.user_id and ug.is_voided is false\n" +
-            "         left join groups g on ug.group_id = g.id\n" +
-            "         left join group_privilege gp on ug.id = gp.group_id and gp.is_voided is false\n" +
-            "         left join privilege p on gp.privilege_id = p.id and p.name = 'Analytics'\n" +
-            "where u.uuid = '%s'\n" +
-            "group by u.id, u.username, u.uuid, u.organisation_id;";
-
-    private final String FIND_BY_USERNAME = "select u.id,\n" +
-            "       u.username username,\n" +
-            "       u.uuid uuid,\n" +
-            "       u.organisation_id organisation_id,\n" +
-            "       case when count(p.id) > 0 or bool_or(g.has_all_privileges) = true then true else false end as has_analytics_access,\n" +
-            "       case when count(aa.id) > 0 then true else false end                                        as is_admin\n" +
-            "from users u\n" +
-            "         left join account_admin aa on u.id = aa.admin_id\n" +
-            "         left join user_group ug on u.id = ug.user_id and ug.is_voided is false\n" +
-            "         left join groups g on ug.group_id = g.id\n" +
-            "         left join group_privilege gp on ug.id = gp.group_id and gp.is_voided is false\n" +
-            "         left join privilege p on gp.privilege_id = p.id and p.name = 'Analytics'\n" +
-            "where u.username = '%s'\n" +
-            "group by u.id, u.username, u.uuid, u.organisation_id;";
+    private final String FIND_BY_UUID = readFile("sql/api/findByUuid.sql");
+    private final String FIND_BY_USERNAME = readFile("sql/api/findByUsername.sql");
 
     private User user;
     private final JdbcTemplate jdbcTemplate;
@@ -54,9 +31,10 @@ public class UserRepository {
     }
 
     public User findByUsername(String username) {
-        String sql = format(FIND_BY_USERNAME, username);
         try {
-            jdbcTemplate.queryForObject(sql, (rs, rowNum) -> setUser(rs));
+            Map<String, Object> parameters = new HashMap<>(1);
+            parameters.put("username", username);
+            new NamedParameterJdbcTemplate(jdbcTemplate).queryForObject(FIND_BY_USERNAME, parameters, (rs, rowNum) -> setUser(rs));
         }
         catch (EmptyResultDataAccessException emptyResultDataAccessException) {
             return null;
@@ -65,9 +43,11 @@ public class UserRepository {
     }
 
     public User findByUuid(String userUUID) {
-        String sql = format(FIND_BY_UUID, userUUID);
+        Map<String, Object> parameters = new HashMap<>(1);
+        parameters.put("uuid", userUUID);
 
-        jdbcTemplate.queryForObject(sql, (rs, rowNum) -> setUser(rs));
+        new NamedParameterJdbcTemplate(jdbcTemplate).queryForObject(FIND_BY_UUID, parameters, (rs, rowNum) -> setUser(rs));
+
         return this.user;
     }
 
