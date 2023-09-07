934b5d0698928ba811540a9613644afd88e90d8d
Vinay Venu <ijkvinay@gmail.com>
2023-09-07 11:40:58 +0530

    #69 | Fix join condition

--- src/main/resources/sql/api/findByUuid.sql
+++ src/main/resources/sql/api/findByUuid.sql
@@ -8,7 +8,7 @@ from users u
          left join account_admin aa on u.id = aa.admin_id
          left join user_group ug on u.id = ug.user_id and ug.is_voided is false
          left join groups g on ug.group_id = g.id
-         left join group_privilege gp on ug.id = gp.group_id and gp.is_voided is false
+         left join group_privilege gp on g.id = gp.group_id and gp.is_voided is false
          left join privilege p on gp.privilege_id = p.id and p.name = 'Analytics'
 where u.uuid = :uuid
 group by u.id, u.username, u.uuid, u.organisation_id;
