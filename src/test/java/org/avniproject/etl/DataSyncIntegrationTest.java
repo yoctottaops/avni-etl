f212e7fd88598b807279ec86ebddc247559755da
vedfordev <vedforworks@gmail.com>
2024-04-12 16:58:08 +0530

    #94 | changed method and variable name

--- src/test/java/org/avniproject/etl/DataSyncIntegrationTest.java
+++ src/test/java/org/avniproject/etl/DataSyncIntegrationTest.java
@@ -355,10 +355,10 @@ public class DataSyncIntegrationTest extends BaseIntegrationTest {
     @Test
     @Sql({"/test-data-teardown.sql", "/test-data.sql"})
     @Sql(scripts = "/test-data-teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    public void userTableShouldUpdateWithOldUserDataandIsVoided() throws InterruptedException {
+    public void userTableShouldUpdateWithIsVoided() throws InterruptedException {
         runDataSync();
-        String updateLastModifiedDateTimeSql = "update public.users set last_modified_date_time = now(),is_voided = true where id = 3453;";
-        jdbcTemplate.execute(updateLastModifiedDateTimeSql);
+        String updateIsVoided = "update public.users set last_modified_date_time = now(),is_voided = true where id = 3453;";
+        jdbcTemplate.execute(updateIsVoided);
 
         runDataSync();
         List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from orgc.users where is_voided = true and id = 3453 ; ");
